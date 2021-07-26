cce1aecd926cba3f8a21ddd0c2a71e1c5adb76c4
Sunspire <gfdavid@gmail.com>
2021-07-26 19:18:53 +0400

    work in progress

--- classes/item.py
+++ classes/item.py
@@ -1,4 +1,8 @@
 class item():
-    def __init__(self, name, description):
+    def __init__(self, name, description, value=0):
         self.name = name
         self.description = description
+
+class weapon(item):
+    def __init__(self, name, description, value):
+        super().__init__(name, description, value=value)
