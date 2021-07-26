f8fa78a6a81f381970a8ba9f7c5e29cf18c71cb2
Sunspire <gfdavid@gmail.com>
2021-07-26 19:18:32 +0400

    Removed unused code and added a new color code to the output function

--- functions/general.py
+++ functions/general.py
@@ -1,8 +1,6 @@
-from classes.action import action
-from classes.player import player
-
 def output(text, colorCode):
     color_code = {}
+    color_code['reset'] = 0 #resets all colors
     color_code['black'] = 30
     color_code['red'] = 31
     color_code['green'] = 32
@@ -11,7 +9,7 @@ def output(text, colorCode):
     color_code['magenta'] = 35
     color_code['cyan'] = 36
     color_code['gray'] = 37
-
+    
     if color_code.get(colorCode) is None:
         return '\33[31mfuntions > general > output > Unknown color'
     else:
