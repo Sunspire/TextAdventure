eaf3ec058029a16d10d0c33364eeb5fb00767c71
Sunspire <gfdavid@gmail.com>
2021-07-26 19:20:28 +0400

    Removed unused code.
    Improved the logic (WIP)

--- main.py
+++ main.py
@@ -14,41 +14,31 @@ actions = action_list()
 def evaluate_command(the_input):
     the_input = the_input.strip().lower()
     if the_input != '':
-        global the_player
         command_not_found = True
         
         if the_input == 'exit':
-            confirm_exit = input(output('are you sure (y/n)? > '.format(the_player), 'yellow'))
+            confirm_exit = input(output('are you sure (y/n)? > ', 'yellow'))
             if confirm_exit.strip().lower() == 'y':
                 global is_exit
                 is_exit = 1
+                print(output('', 'reset'))
+            else:
+                print(output('Returning to game.', 'green'))
         else:
+            first_word_in_command = the_input.split()[0]
             for i in range(len(actions)):
-                if the_input == actions[i].command:
+                if first_word_in_command == actions[i].command:
+                    the_player.command = the_input
                     the_player.do_action(actions[i], the_world)
                     command_not_found = False
                     break
 
             if (command_not_found):
                 print(output('Unknown command', 'red'))
-                
-while is_exit == 0:    
-    print(output("({},{})".format(the_player.x, the_player.y), 'green'))
-    command = input(output('{} > '.format(the_player), 'cyans'))
-    evaluate_command(command)
+#testing
+the_world.test_override()
 
-'''
-        if input == 'w': new_y = y - 1
-        if input == 's': new_y = y + 1
-        if input == 'a': new_x = x - 1
-        if input == 'd': new_x = x + 1
-        new_position = (new_x, new_y)
-        
-        if world.get(new_position):
-            x = new_x
-            y = new_y
-            position = new_position
-            print(world.get(position))
-        else:
-            print('you cannot move there')
-'''
+while is_exit == 0:    
+    #print(output("({},{})".format(the_player.x, the_player.y), 'green'))
+    command = input(output('{} > '.format(the_player), 'cyan'))
+    evaluate_command(command)
