2e0309f1b4e37dd1fd93d1ece5b5fa19feafa446
Sigmanificient <edhyjox@gmail.com>
2021-07-26 20:21:07 +0200

    simplified output from general.py ps: considering using a module such as colorama/termcolor could be a good idea

---
+++ functions/general.py
@@ -0,0 +1,18 @@
+color_codes = {
+    'reset': 0,
+    'black': 30,
+    'red': 31,
+    'green': 32,
+    'yellow': 33,
+    'blue': 34,
+    'magenta': 35,
+    'cyan': 36,
+    'gray': 37
+}
+
+
+def output(text, color_code):
+    if color_codes.get(color_code) is None:
+        return '\33[31mfuntions > general > output > Unknown color'
+
+    return f'\33[{color_codes.get(color_code)}m{text}'
