fdf7ac19efffb9ffcffb3ca416dc9398f1978be8
Sunspire <gfdavid@gmail.com>
2021-07-26 19:17:19 +0400

    Converted the world tile to a list (it was just a text)
    Added a test function to test various interactions

--- classes/world.py
+++ classes/world.py
@@ -1,3 +1,5 @@
+from classes.item import item
+
 class world():
     def __init__(self):
         self.tiles = {}
@@ -10,10 +12,18 @@ class world():
             for yy in range(len(rows)):
                 cols = rows[yy].split('\t')
                 for xx in range(x_max):
-                    self.tiles[(xx, yy)] = cols[xx].replace('\n', '')
+                    self.tiles[(xx, yy)] = [cols[xx].replace('\n', '')]
 
     def tile_exists(self, coords=(-1, -1)):
         if self.tiles.get(coords) is None:
             return False
         else:
             return self.tiles.get(coords) is not None
+
+    def test_override(self):
+        cup = item('cup', 'some foul smelling liquid is inside')
+        book = item('book', 'it contains drawings of legendary creatures')
+        the_items = [cup, book]
+        self.tiles[(0,0)] = ['''You see a sign with the word "TEST" written on it.
+Various items are floating in the air.
+In the distance you see nothing but darkness.''', the_items]
