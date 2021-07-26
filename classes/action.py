bf356148e3e3fadff8d9167ba4eb0bada7943b27
Sunspire <gfdavid@gmail.com>
2021-07-26 19:15:13 +0400

    removed unused import and added more actions

--- classes/action.py
+++ classes/action.py
@@ -1,4 +1,3 @@
-from classes.item import item
 from classes.player import player
 
 class action():
@@ -13,4 +12,7 @@ def action_list():
     act_list.append(action(method=player.move_south, name='Move south', command='s'))
     act_list.append(action(method=player.move_east, name='Move east', command='d'))
     act_list.append(action(method=player.move_west, name='Move west', command='a'))
+    act_list.append(action(method=player.look, name='Look', command='look'))
+    act_list.append(action(method=player.examine_item, name='Examine item', command='examine'))
+    act_list.append(action(method=player.examine_item, name='Examine item', command='exam'))
     return act_list
